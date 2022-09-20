bae88a42ee395aa2994dbf637dfafe8fd39e3607
Merzaad <Merzaad@gmail.com>
2022-09-20 20:01:29 +0430

    menu button

--- src/App.tsx
+++ src/App.tsx
@@ -44,7 +44,7 @@ function App() {
               <img src="/react.svg" className="logo react" alt="React logo" />
             </a>
           </div>
-          <h1 className="header">reactVite v0.0.2</h1>
+          <h1 className="header">reactVite v0.0.3</h1>
           <div className="box">
             <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
           </div>
--- src/components/menu/index.tsx
+++ src/components/menu/index.tsx
@@ -2,7 +2,13 @@ import React from 'react'
 import './index.css'
 
 const Menu = () => {
-  return <div className="Menu"></div>
+  return (
+    <div className="Menu">
+      <button type="button" onClick={() => window.location.reload()}>
+        Reload
+      </button>
+    </div>
+  )
 }
 
 export default Menu
