fc2d52cd12961afad0136700f1dacafb4e9fd3b3
merzaad <merzaad@gmail.com>
2022-12-15 09:48:20 +0330

    converter

--- src/components/x/index.tsx
+++ src/components/x/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import './index.css'
 import { moduleValue, increaseModuleValue } from '../../modules/module'
+import axios from 'axios'
 
 const X = () => {
   const [state, setState] = React.useState(moduleValue)
@@ -9,8 +10,11 @@ const X = () => {
     asset: '',
     quote: '',
     error: { hasError: false, errorMessage: '' },
+    status: 'initial',
   })
-
+  const [pair, setPair] = React.useState({ asset: 'USDT', quote: 'BTC' })
+  const [coins] = React.useState(['BTC', 'DOGE', 'ETH', 'USDT'])
+  const fetching = input.status === 'fetching'
   const increaseHandler = () => {
     increaseModuleValue()
     setState(moduleValue)
@@ -21,6 +25,7 @@ const X = () => {
         asset: String(value),
         quote: String(value / testRatio),
         error: { hasError: value !== 0 && value < 100, errorMessage: '<100' },
+        status: '',
       })
     }
   }
@@ -30,6 +35,7 @@ const X = () => {
         asset: String(value * testRatio),
         quote: String(value),
         error: { hasError: value !== 0 && value * testRatio < 100, errorMessage: '<100' },
+        status: '',
       })
     }
   }
@@ -44,7 +50,31 @@ const X = () => {
   const switchHandler = (): void => {
     setRatio(1 / ratio)
     setAsset(Number(input.quote), 1 / ratio)
+    setPair({ asset: pair.quote, quote: pair.asset })
   }
+  const selectAssetHandler: React.ChangeEventHandler<HTMLSelectElement> = (event) => {
+    const { value } = event.target
+    setPair({ ...pair, asset: value })
+  }
+  const selectQuoteHandler: React.ChangeEventHandler<HTMLSelectElement> = (event) => {
+    const { value } = event.target
+    setPair({ ...pair, quote: value })
+  }
+  React.useEffect(() => {
+    const fetchPrice = async () => {
+      try {
+        setInput({ ...input, status: 'fetching' })
+        const { data } = await axios.get(
+          `https://api.twox.ir/api/currencies/prices/latest/${pair.quote}/${pair.asset}`
+        )
+        setRatio(data.price)
+        setInput({ ...input, status: '' })
+      } catch (error) {
+        console.warn(error)
+      }
+    }
+    fetchPrice()
+  }, [pair])
   return (
     <div className="X">
       <div className="box">
@@ -57,27 +87,55 @@ const X = () => {
         </button>
       </div>
       <div className="box">
-        <input
-          className="input"
-          value={input.asset}
-          type="number"
-          onChange={assetChangeHanler}
-          placeholder="asset"
-        />
-        <button type="button" onClick={switchHandler}>
+        <div>
+          <input
+            className="input"
+            value={fetching ? '' : input.asset}
+            type="number"
+            onChange={assetChangeHanler}
+            placeholder={fetching ? 'fetching' : 'asset'}
+            disabled={fetching}
+          />
+          <select value={pair.asset} className="select" onChange={selectAssetHandler}>
+            {coins.map(
+              (coin) =>
+                coin !== pair.quote && (
+                  <option value={coin} key={`asset ${coin}`}>
+                    {coin}
+                  </option>
+                )
+            )}
+          </select>
+        </div>
+        <button type="button" onClick={switchHandler} disabled={fetching} className="switch">
           switch
         </button>
-        <input
-          className="input"
-          type="number"
-          value={input.quote}
-          onChange={quoteChangeHanler}
-          placeholder="quote"
-        />
-        <div className='help'>
+        <div>
+          <input
+            className="input"
+            type="number"
+            value={fetching ? '' : input.quote}
+            onChange={quoteChangeHanler}
+            placeholder={fetching ? 'fetching' : 'quote'}
+            disabled={fetching}
+          />
+          <select value={pair.quote} className="select" onChange={selectQuoteHandler}>
+            {coins.map(
+              (coin) =>
+                coin !== pair.asset && (
+                  <option value={coin} key={`asset ${coin}`}>
+                    {coin}
+                  </option>
+                )
+            )}
+          </select>
+        </div>
+        <div className="help">
           error: {input.error.hasError && input.error.errorMessage}
           <br />
-          ratio: {ratio}
+          price: {fetching ? 'fetching' : `${ratio} ${pair.quote}${pair.asset}`}
+          <br />
+          status: {input.status}
         </div>
       </div>
     </div>
