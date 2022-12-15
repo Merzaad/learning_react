4f294858a81f2b8e5465e8e92c4cb2e3223ad93d
merzaad <merzaad@gmail.com>
2022-12-15 09:50:57 +0330

    disable sw

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,16 @@ import React from 'react'
 import Notification from './components/notification'
 import notfContext from './context/notifContext'
 import Layout from './components/layout'
+// eslint-disable-next-line no-unused-vars
 import { register } from './swRegisteration'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './components/app'
 import X from './components/x'
 
 function App() {
+  // eslint-disable-next-line no-unused-vars
   const [update, setUpdate] = React.useState(false)
-  React.useEffect(() => {
+  /*   React.useEffect(() => {
     register({
       onUpdate: (registration: any) => {
         if (registration && registration.waiting) {
@@ -18,7 +20,7 @@ function App() {
         setUpdate(true)
       },
     })
-  })
+  }) */
   return (
     <>
       <BrowserRouter>
