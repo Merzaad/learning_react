e783790b21e2f6f99e39c1b843031085dce6a8a7
merzaad <merzaad@gmail.com>
2023-02-02 12:31:12 +0330

    useCallback staling closure?

--- src/components/y/button.tsx
+++ src/components/y/button.tsx
@@ -2,12 +2,20 @@ import * as React from 'react'
 
 export default function Button() {
   const [state, setState] = React.useState(0)
-  console.log(state)
-  const clickHandler = React.useCallback(() => setState((x) => x + 1), [])
+  const [state2, setState2] = React.useState(0)
+  const [triggerCallBack, setTriggerCallBack] = React.useState(false)
+  const clickHandler = React.useCallback(() => {
+    setState((x) => x + state2)
+    console.log(state2)
+  }, [triggerCallBack])
+  const oldSatate = React.useMemo(() => state2, [triggerCallBack])
   return (
     <>
       {state}
-      <button onClick={clickHandler}>memo preventing re-render</button>
+      <button onClick={clickHandler}>add {oldSatate}</button>
+      <button onClick={() => setTriggerCallBack((x) => !x)}>update callback</button>
+      <button onClick={() => setState2((x) => x + 1)}>increase {state2}</button>
+      {'memeo prevent re-render when parent renders'}
     </>
   )
 }
