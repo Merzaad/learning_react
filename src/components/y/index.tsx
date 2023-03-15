d79a35671cd7bb476e90d45c316b5bef16f85694
merzaad <merzaad@gmail.com>
2023-03-16 02:53:28 +0330

    stale

--- src/components/y/index.tsx
+++ src/components/y/index.tsx
@@ -13,7 +13,21 @@ const reducer: React.Reducer<number, React.ReducerAction<Reducer>> = (state, act
 }
 export default function Y() {
   const [state, dispatch] = React.useReducer(reducer, 0)
+  const [staled, setStaled] = React.useState(0)
+  const testStale = 1 + staled
+  const testLocalstorageStale = localStorage.getItem('stale') || 0
+  const staledCallback = React.useCallback(() => {
+    const testStale2 = 1 + staled
+    const testLocalstorageStale2 = localStorage.getItem('stale') || 0
+    console.log(testStale, testStale2, testLocalstorageStale, testLocalstorageStale2)
+  }, [])
+  const onStaledIncrease = () => {
+    setStaled((prev) => prev + 1)
+  }
   const preventReRender = React.useMemo(() => <Button />, [])
+  React.useEffect(() => {
+    localStorage.setItem('stale', String(staled))
+  }, [staled])
   return (
     <div className="y">
       <div className="box">
@@ -25,6 +39,10 @@ export default function Y() {
       <div className="box">
         module:{moduleValue}
         <button onClick={() => increaseModuleValue()}>+1</button>
+      </div>
+      <div className="box">
+        {staled} <button onClick={onStaledIncrease}>+</button>
+        <button onClick={staledCallback}>print</button>
       </div>
     </div>
   )
